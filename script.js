64d2e8767db38ad7ba65ad453c4f7a029de2f21d
Clara Smyth <clarasmyth.work@gmail.com>
2022-06-12 16:12:51 +0100

    Finished the function that prints books

--- script.js
+++ script.js
@@ -36,12 +36,55 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-// Prints the library to the page
-function addBooksToPage() {
+// Prints the book to the page
+function addBookToPage() {
   const bookSection = document.querySelector('.main');
-  const newDiv = document.createElement('div');
-  newDiv.classList.add('card')
-  bookSection.append(newDiv);
+
+  // Creates the card div
+  const newCardDiv = document.createElement('div');
+  newCardDiv.classList.add('card')
+
+  //Adds a title to the div
+  const cardTitle = document.createElement('h2');
+  cardTitle.classList.add('card-title');
+  cardTitle.innerText = 'Title';
+  newCardDiv.append(cardTitle);
+
+  // Adds the author name to the div
+  const cardAuthor = document.createElement('p');
+  cardAuthor.classList.add('card-author');
+  cardAuthor.innerText = 'Author Name';
+  newCardDiv.append(cardAuthor);
+
+  // Adds the pages to the div
+  const cardPages = document.createElement('p');
+  cardPages.classList.add('card-pages');
+  cardPages.innerText = '500';
+  newCardDiv.append(cardPages);
+
+  // Adds the read checkbox to the div
+  const cardCheckbox = document.createElement('input');
+  cardCheckbox.setAttribute('type', 'checkbox');
+  cardCheckbox.setAttribute('id', 'checkbox');
+  cardCheckbox.setAttribute('name', 'checkbox');
+  cardCheckbox.classList.add('card-checkbox');
+  newCardDiv.append(cardCheckbox);
+
+  // Adds the read checkbox label to the div
+  const cardCheckboxLabel = document.createElement('label');
+  cardCheckboxLabel.setAttribute('for', 'checkbox');
+  cardCheckboxLabel.classList.add('card-checkbox-label');
+  cardCheckboxLabel.innerText = 'Read?';
+  newCardDiv.append(cardCheckboxLabel);
+
+  // Adds a remove button to the div
+  const cardBtn = document.createElement('button');
+  cardBtn.classList.add('card-btn');
+  cardBtn.innerText = 'Remove';
+  newCardDiv.append(cardBtn);
+
+  //Adds the new book to the page
+  bookSection.append(newCardDiv);
 }
 
-addBooksToPage()
+addBookToPage()
