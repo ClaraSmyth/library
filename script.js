24f0430754d6ef923140e3cdb981bd885e873623
Clara Smyth <clarasmyth.work@gmail.com>
2022-06-12 18:13:06 +0100

    Linked the remove buttons to book index

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ const bookSection = document.querySelector('.main');
 // Prints all the books in the Library to the page
 function printBooks() {
   bookSection.textContent = '';
-  myLibrary.forEach((book) => {
-    createBookCard(book);
+  myLibrary.forEach((book, index) => {
+    createBookCard(book, index);
+    console.log(book, index)
   });
 }
 
@@ -18,7 +19,6 @@ bookForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const book = new Book(title.value, author.value, pages.value, read.checked);
   myLibrary.push(book);
-  printBooks();
   console.log(myLibrary)
 });
 
@@ -47,8 +47,7 @@ function Book(title, author, pages, read) {
 }
 
 // Creates the Book elements for the page
-function createBookCard(book) {
-
+function createBookCard(book, index) {
   // Creates the card div
   const newCardDiv = document.createElement('div');
   newCardDiv.classList.add('card')
@@ -88,6 +87,7 @@ function createBookCard(book) {
 
   // Adds a remove button to the div
   const cardBtn = document.createElement('button');
+  cardBtn.setAttribute('data', index);
   cardBtn.classList.add('card-btn');
   cardBtn.innerText = 'Remove';
   newCardDiv.append(cardBtn);
@@ -97,3 +97,4 @@ function createBookCard(book) {
 }
 
 printBooks();
+// console.log(myLibrary[1])
